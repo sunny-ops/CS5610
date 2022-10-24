ddcbe4da36eb32999112f293d7fe1eb332853e68
shizhong <>
2022-10-23 20:22:36 -0700

    Init repo

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <div class="Container">
+        <div class="Header">Shi Zhong's Website</div>
+        <div class="Left"></div>
+        <div class="Main">
+          <div class="Box"></div>
+        </div>
+        <div class="Right"></div>
+        <div class="Footer"></div>
+
+      </div>
+
+    </body>
+  );
+}
+
+export default App;
